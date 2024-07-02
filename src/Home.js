ec6d5263ae3cb0fe2decf4f03d3646371f195f12
Benoit <bmouly76@gmail.com>
2024-07-02 15:12:02 +0200

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>My Application</h1>
+      <img src="path_to_logo" alt="Logo" style={{ width: '200px', height: '200px' }} />
+      <p>Welcome to my application!</p>
+    </div>
+  );
+}
+
+export default Home;
